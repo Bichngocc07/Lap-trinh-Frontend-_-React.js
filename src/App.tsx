2c524825195d930782d75147665f8778c53aa39c
Bichngocc07 <ngoc01072006@gmail.com>
2025-08-20 15:48:45 +0700

    First commit - push React project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import SESSION13 from "./Session13";
+
+function App() {
+  return (
+    <div>
+      <h1>Demo React với Class Component</h1>
+      <SESSION13 />
+    </div>
+  );
+}
+
+export default App;
